c93bf4e6a130b7fdec77212a0d46e1eeaf41f3d5
Mason Jennings <mjennings@pinterest.com>
2018-10-21 16:29:07 -0700

    Add json for 2018 races so far

---
+++ src/data/JSON/Seasons/2018/index.js
@@ -0,0 +1,37 @@
+import GP01AustralianGrandPrix from './01-AustralianGrandPrix.json';
+import GP02BahrainGrandPrix from './02-BahrainGrandPrix.json';
+import GP03ChineseGrandPrix from './03-ChineseGrandPrix.json';
+import GP04AzerbaijanGrandPrix from './04-AzerbaijanGrandPrix.json';
+import GP05SpanishGrandPrix from './05-SpanishGrandPrix.json';
+import GP06MonacoGrandPrix from './06-MonacoGrandPrix.json';
+import GP07CanadianGrandPrix from './07-CanadianGrandPrix.json';
+import GP08FrenchGrandPrix from './08-FrenchGrandPrix.json';
+import GP09AustrianGrandPrix from './09-AustrianGrandPrix.json';
+import GP10BritishGrandPrix from './10-BritishGrandPrix.json';
+import GP11GermanGrandPrix from './11-GermanGrandPrix.json';
+import GP12HungarianGrandPrix from './12-HungarianGrandPrix.json';
+import GP13BelgianGrandPrix from './13-BelgianGrandPrix.json';
+import GP14ItalianGrandPrix from './14-ItalianGrandPrix.json';
+import GP15SingaporeGrandPrix from './15-SingaporeGrandPrix.json';
+import GP16RussianGrandPrix from './16-RussianGrandPrix.json';
+import GP17JapaneseGrandPrix from './17-JapaneseGrandPrix.json';
+
+module.exports = [
+  GP01AustralianGrandPrix,
+  GP02BahrainGrandPrix,
+  GP03ChineseGrandPrix,
+  GP04AzerbaijanGrandPrix,
+  GP05SpanishGrandPrix,
+  GP06MonacoGrandPrix,
+  GP07CanadianGrandPrix,
+  GP08FrenchGrandPrix,
+  GP09AustrianGrandPrix,
+  GP10BritishGrandPrix,
+  GP11GermanGrandPrix,
+  GP12HungarianGrandPrix,
+  GP13BelgianGrandPrix,
+  GP14ItalianGrandPrix,
+  GP15SingaporeGrandPrix,
+  GP16RussianGrandPrix,
+  GP17JapaneseGrandPrix,
+];

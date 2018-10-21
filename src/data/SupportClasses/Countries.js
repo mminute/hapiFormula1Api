e3cfd10d126a1197ef876b1599f7e97983d5cd48
Mason Jennings <mjennings@pinterest.com>
2018-10-21 16:27:52 -0700

    Add support classes

---
+++ src/data/SupportClasses/Countries.js
@@ -0,0 +1,26 @@
+class Country {
+  constructor(props) {
+    this.name = props.name;
+  }
+}
+
+module.exports = {
+  France: new Country({ name: 'France' }),
+  NewZealand: new Country({ name: 'New Zealand' }),
+  Monaco: new Country({ name: 'Monaco' }),
+  Russia: new Country({ name: 'Russia' }),
+  UnitedKingdom: new Country({ name: 'United Kingdom' }),
+  Finland: new Country({ name: 'Finland' }),
+  Germany: new Country({ name: 'Germany' }),
+  Australia: new Country({ name: 'Australia' }),
+  Netherlands: new Country({ name: 'Netherlands' }),
+  Mexico: new Country({ name: 'Mexico' }),
+  Brazil: new Country({ name: 'Brazil' }),
+  Canada: new Country({ name: 'Canada' }),
+  Spain: new Country({ name: 'Spain' }),
+  Denmark: new Country({ name: 'Denmark' }),
+  Belgium: new Country({ name: 'Belgium' }),
+  Sweden: new Country({ name: 'Sweden' }),
+  Italy: new Country({ name: 'Italy' }),
+  USA: new Country({ name: 'United States of America' }),
+};

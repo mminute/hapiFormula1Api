bea2637dff4361a518b44ef9939b274821d5e227
Mason Jennings <mjennings@pinterest.com>
2018-10-29 22:24:44 -0700

    Update Season Controller to find season data for request param

--- src/controllers/Season.js
+++ src/controllers/Season.js
@@ -1,6 +1,8 @@
+import Seasons from '../data/JSON/Seasons/index';
+
 const SeasonController = (request, h) => {
-  console.log('request.params.year', request.params.year);
-  return { hello: 'world' };
+  console.log('request.params.event?', request.params.event);
+  return Seasons[request.params.year] || [];
 };
 
 export default SeasonController;
